489a2b24a847bd8ad287e0e32e1e2592df12d469
AnasIshfaque <aosmani203004@bscse.uiu.ac.bd>
2024-05-12 18:01:45 +0600

    FL TL with MedMNIST code added

--- Fed_Trans_learning/MedMNIST/MedMNIST_SqueezeNet_fd_client_rasp.py
+++ Fed_Trans_learning/MedMNIST/MedMNIST_SqueezeNet_fd_client_rasp.py
@@ -16,6 +16,12 @@ users = 3 # number of clients
 import os
 import h5py
 import numpy as np
+import subprocess
+import time
+import copy
+import random
+import shutil
+import glob
 
 import socket
 import struct
@@ -27,17 +33,24 @@ import torch.nn.functional as F
 import torchvision
 from torchvision import datasets,transforms,models
 import torch.optim as optim
-
+from torch.autograd import Variable
+from torch.optim import Adam, lr_scheduler
+from torch import tensor
 from torch.utils.data import Dataset, DataLoader
-
-import time
-import copy
+import torchmetrics
+from torchmetrics.classification import Accuracy, Precision, Recall, F1Score
 
 from tqdm import tqdm
-
+from pathlib import Path
+from gpiozero import CPUTemperature
+import medmnist
+from medmnist import INFO, Evaluator
+from datetime import datetime
 
 # In[2]:
 
+dataset_name = "pneumoniamnist"
+model_name = "squeezenet"
 
 def getFreeDescription():
     free = os.popen("free -h")
@@ -58,8 +71,6 @@ def getFree():
         if i == 2:
             return (line.split()[0:7])
 
-from gpiozero import CPUTemperature
-
 
 def printPerformance():
     cpu = CPUTemperature()
@@ -77,55 +88,47 @@ def printPerformance():
     print(description[5] + " : " + mem[6])
 
 
-# In[3]:
-
-
 printPerformance()
 
+subprocess.Popen(["./check_device.sh"])
 
-# In[5]:
+start_time = datetime.now()
+start_time = start_time.strftime("%H:%M:%S")
+print("Main code exe started at ", start_time)
 
-
-root_path = '../../datasets/THB_splitted'
-
-
-# ## Cuda
-
-# In[6]:
-
+torch.manual_seed(42)
 
 # device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 device = "cpu"
-print(device)
-
-
-# In[7]:
-
 
 client_order = int(input("client_order(start from 0): "))
 
 
-# In[8]:
-
-
-num_traindata = 262 // users
+num_traindata = 4708 // users # divide training instances evenly to each clients
 
 
 # ## Data load
 
-# In[9]:
-
-
-# transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2470, 0.2435, 0.2616))])
-
 mean = np.array([0.485,0.456,0.406])
 std = np.array([0.229,0.224,0.225])
-transform = transforms.Compose([transforms.RandomResizedCrop(224),transforms.RandomHorizontalFlip(),transforms.ToTensor(),transforms.Normalize(mean,std)])
+data_transforms = {
+  'train':transforms.Compose([
+      transforms.Grayscale(num_output_channels=3),  # Convert grayscale to RGB
+      transforms.RandomResizedCrop(224),
+      transforms.RandomHorizontalFlip(),
+      transforms.ToTensor(),
+      transforms.Normalize(mean,std)
+  ]),
+  'test':transforms.Compose([
+      transforms.Grayscale(num_output_channels=3),  # Convert grayscale to RGB
+      transforms.Resize(256),
+      transforms.CenterCrop(224),
+      transforms.ToTensor(),
+      transforms.Normalize(mean,std)
+  ])
+}
 
-from torch.utils.data import Subset
-
-
-indices = list(range(262))
+indices = list(range(4708))
 
 lower_idx = num_traindata * client_order
 upper_idx = num_traindata * (client_order + 1)
@@ -136,50 +139,34 @@ if (client_order+1 == users):
     
 part_tr = indices[lower_idx : upper_idx]
 
+# Loading MedMNIST dataset
+info = INFO['pneumoniamnist']
+task = info['task']
+n_channels = info['n_channels']
+n_classes = len(info['label'])
 
-# In[10]:
+DataClass = getattr(medmnist, info['python_class'])
+class_labels = info['label']
+print(info['label'])
 
+BATCH_SIZE = 128
 
-# trainset = torchvision.datasets.CIFAR10 (root=root_path, train=True, download=True, transform=transform)
-trainset = datasets.ImageFolder(os.path.join(root_path,'train'), transform)
+sets = ['train','test']
+image_datasets = {x:DataClass(split=x, transform=data_transforms[x], download=True, size=128)
+                for x in ['train','test']}
 
-trainset_sub = Subset(trainset, part_tr)
+trainset_sub = Subset(image_datasets['train'], part_tr)
 
-print(f'trainset size: {len(trainset)}, trainset_sub: {len(trainset_sub)}')
+print(f'trainset size: {len(image_datasets['train'])}, trainset_sub: {len(trainset_sub)}')
 train_loader = torch.utils.data.DataLoader(trainset_sub, batch_size=4, shuffle=True, num_workers=0)
 
-# testset = torchvision.datasets.CIFAR10 (root=root_path, train=False, download=True, transform=transform)
-# test_loader = torch.utils.data.DataLoader(testset, batch_size=4, shuffle=False, num_workers=0)
-
-
-# In[11]:
-
-# classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
 classes = ('Bluetooth', 'Humidity', 'Transistor')
 
 # ### Number of total batches
-
-# In[13]:
-
-
 train_total_batch = len(train_loader)
 print(f'len(train_loader): {train_total_batch}')
-# test_batch = len(test_loader)
-# print(test_batch)
 
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Nov  1 14:23:31 2018
-@author: tshzzz
-"""
-
-import torch
-import torch.nn as nn
-
-# In[15]:
-
-
-sq_model = models.squeezenet1_1(weights=True)
+sq_model = models.squeezenet1_1(weights=SqueezeNet1_1_Weights.DEFAULT)
 sq_model.to(device)
 
 #freezing previous layers
@@ -203,7 +190,7 @@ lr = 0.01
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.SGD(sq_model.parameters(), lr=lr, momentum=0.9)
 
-rounds = 400 # default
+rounds = 10 # default
 local_epochs = 1 # default
 
 
@@ -243,41 +230,23 @@ def recvall(sock, n):
     return data
 
 
-# In[15]:
-
-
 printPerformance()
 
 
 # ### Set host address and port number
-
-# In[18]:
-
-
 host = input("IP address: ")
 port = 10080
 max_recv = 100000
 
 
 # ### Open the client socket
-
-# In[19]:
-
-
 s = socket.socket()
 s.connect((host, port))
 
 
 # ## SET TIMER
-
-# In[20]:
-
-
 start_time = time.time()    # store start time
 print("timmer start!")
-
-
-# In[21]:
 
 
 msg = recv_msg(s)
@@ -323,22 +292,8 @@ for r in range(rounds):  # loop over the dataset multiple times
 
 print('Finished Training')
 
-
-# In[ ]:
-
-
 printPerformance()
-
-
-# In[23]:
-
 
 end_time = time.time()  #store end time
 print("Training Time: {} sec".format(end_time - start_time))
-
-
-# In[ ]:
-
-
-
 
